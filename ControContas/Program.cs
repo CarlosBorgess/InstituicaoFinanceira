037a7ad8a771ebaffdaddb83385bd681fedef090
CarlosBorgess <carloseduana2004@gmail.com>
2025-09-30 20:55:52 -0300

    Aula 30/09 2

--- ControContas/Program.cs
+++ ControContas/Program.cs
@@ -10,3 +10,4 @@ Console.WriteLine($"O numero da conta 2 é {conta2.Numero}");
 Console.WriteLine($"O valor do saldo é ${conta2.Saldo} reais");
 
 //Crie um atributo saldo na classe conta cuja a propriedade seja somente leitura 
+//Crie um atributo saldo na classe Conta cuja propriedade de teste para testar a classe Conta ele deverá testar um método depositar 
